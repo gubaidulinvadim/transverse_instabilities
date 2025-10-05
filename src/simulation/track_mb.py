812996432da188e27d97112b634449010e9ebefa
gubaidulinvadim <gubaidulinvadim@gmail.com>
2025-10-05 23:07:26 +0200

    Adapted to facilities_mbtrack2 module

--- src/simulation/track_mb.py
+++ src/simulation/track_mb.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 import numpy as np
 os.environ["PYTHONPATH"] += os.pathsep + "/home/dockeruser/facilities_mbtrack2/"
 sys.path.append('/home/dockeruser/facilities_mbtrack2')

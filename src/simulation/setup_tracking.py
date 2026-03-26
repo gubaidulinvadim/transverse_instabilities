c4a68efb8cec4a2f6a9c6d1b2bdc41fc053d0dad
gubaidulinvadim <gubaidulinvadim@gmail.com>
2026-03-26 15:25:40 +0100

    Correcting a bug on csr_wakefield when csr_flag is False

--- src/simulation/setup_tracking.py
+++ src/simulation/setup_tracking.py
@@ -66,8 +66,8 @@ def setup_wakes(ring, id_state, include_Zlong, n_bin, wake_types='Wydip',
         wakefield_csr = WakePotential(ring,
                                       wakefield=WakeField([wlong_csr, wcsr_csr]),
                                       n_bin=n_bin)
-    # else:
-    # wakefield_csr = None
+    else:
+        wakefield_csr = None
     return wakefield_tr, wakefield_long, wakemodel, wakefield_csr
 
 def setup_wakes_soleil(ring, id_state, include_Zlong, n_bin, wake_types='Wydip',
